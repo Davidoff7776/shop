8708b3b92716767c68993b4d36b9acc4991d8406
Davidoff7776 <47067214+Davidoff7776@users.noreply.github.com>
2019-01-28 17:57:49 +0200

    Update shop.py

--- shop.py
+++ shop.py
@@ -15,49 +15,55 @@ def get_user_input(message, type=str):
             print (f"Please input a {type}.")
 
 
-def add_item(shopping_list):
-    product = get_user_input("Input the product name: ")
-    quantity = get_user_input("Input the product quantity: ", int)
-    shopping_list[product] += quantity
+class ShoppingList:
 
 
-def print_list(shopping_list):
-    for product, quantity in shopping_list.items():
-        print(product, "x", quantity)
+    def __init__(self):
+        self.items = defaultdict(int)
 
 
-def email_to(shopping_list, from_email, password, *recipients):
-    email = EmailMessage()
-    email['Subject'] = "Shopping List"
-    email['From'] = from_email
-    email['To'] = recipients
-    message = "\n".join(f"{product} x {quantity}" for product, quantity in shopping_list.items())
-    email.set_content(message)
+    def __str__(self):
+        return "\n".join(f"{product} x {quantity}" for product, quantity in self.items.items())
 
-    try:
-        s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        s.ehlo()
-        s.login(from_email, password)
-        s.send_message(email)
-        s.quit()
-        print ("\nThe email has been sent.")
-    except Exception as e:  
-        print ("\nAn error occurred:", e)
-   
 
-if __name__ == "__main__":
+    def add_item(self, product, quantity):
+        self.items[product] += quantity
+
+
+    def email_to(self, from_email, password, *recipients):
+        email = EmailMessage()
+        email['Subject'] = "Shopping List"
+        email['From'] = from_email
+        email['To'] = recipients
+        message = str(self)
+        email.set_content(message)
+
+        try:
+            s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+            s.ehlo()
+            s.login(from_email, password)
+            s.send_message(email)
+            s.quit()
+            print ("\nThe email has been sent.")
+        except Exception as e:  
+            print ("\nAn error occurred:", e)
+       
+
+if __name__ == '__main__':
     name = input("Name: ")
     n = get_user_input(f"Hi, {name}!\nHow many products do you want to add to the shopping list? ", int)
-    shopping_list = defaultdict(int)
+    shopping_list = ShoppingList()
     for _ in range(n):
-        add_item(shopping_list)
-    print("\nThese products have been added to the shopping list:")
-    print_list(shopping_list)
+        product = get_user_input("Input the product name: ")
+        quantity = get_user_input("Input the product quantity: ", int)
+        shopping_list.add_item(product, quantity)
+    print("\nThese products have been added to the shopping list:")    
+    print(shopping_list)
 
-    from_email = input("\nEmail: ")
+    email = input("\nEmail: ")
     password = getpass("Password: ")
     recipient = input("Recipient's email: ")
-    email_to(shopping_list, from_email, password, recipient)
+    shopping_list.email_to(email, password, recipient)
 
     print ("\nHave a nice day!")
 
