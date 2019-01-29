f3b4c27b43f7d794d91f231c4de3535f7decf374
Davidoff7776 <47067214+Davidoff7776@users.noreply.github.com>
2019-01-29 14:27:42 +0200

    Update shop.py

--- shop.py
+++ shop.py
@@ -12,23 +12,20 @@ def get_user_input(message, type=str):
         try:
             return type(input(message))
         except ValueError:
-            print (f"Please input a {type}.")
+            print(f"Please input a {type}.")
 
 
 class ShoppingList:
 
-
     def __init__(self):
         self.items = defaultdict(int)
 
-
     def __str__(self):
-        return "\n".join(f"{product} x {quantity}" for product, quantity in self.items.items())
-
+        return ("\n".join(f"{product} x {quantity}"
+                for product, quantity in self.items.items()))
 
     def add_item(self, product, quantity):
         self.items[product] += quantity
-
 
     def email_to(self, from_email, password, *recipients):
         email = EmailMessage()
@@ -44,20 +41,21 @@ class ShoppingList:
             s.login(from_email, password)
             s.send_message(email)
             s.quit()
-            print ("\nThe email has been sent.")
-        except Exception as e:  
-            print ("\nAn error occurred:", e)
-       
+            print("\nThe email has been sent.")
+        except Exception as e:
+            print("\nAn error occurred:", e)
+
 
 if __name__ == '__main__':
     name = input("Name: ")
-    n = get_user_input(f"Hi, {name}!\nHow many products do you want to add to the shopping list? ", int)
+    n = get_user_input(f"Hi, {name}!\nHow many products do you want to add to
+                       the shopping list? ", int)
     shopping_list = ShoppingList()
     for _ in range(n):
         product = get_user_input("Input the product name: ")
         quantity = get_user_input("Input the product quantity: ", int)
         shopping_list.add_item(product, quantity)
-    print("\nThese products have been added to the shopping list:")    
+    print("\nThese products have been added to the shopping list:")
     print(shopping_list)
 
     email = input("\nEmail: ")
@@ -65,6 +63,6 @@ if __name__ == '__main__':
     recipient = input("Recipient's email: ")
     shopping_list.email_to(email, password, recipient)
 
-    print ("\nHave a nice day!")
+    print("\nHave a nice day!")
 
 exit()
