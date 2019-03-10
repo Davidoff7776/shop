074516cdc1a9e2bb9a42b76739e231d7560a7ee7
Davidoff7776 <47067214+Davidoff7776@users.noreply.github.com>
2019-03-10 21:20:09 +0200

    Update

--- shop.py
+++ shop.py
@@ -1,16 +1,15 @@
-import smtplib
+from smtplib import SMTP_SSL
 from email.message import EmailMessage
 from getpass import getpass
 from collections import defaultdict
-import sys
 
 
-def get_user_input(message, type=str):
+def get_user_input(message, category=str):
     while True:
         try:
-            return type(input(message))
+            return category(input(message))
         except ValueError:
-            print(f"Please input a {type}.")
+            print(f"Please input a {category}.")
 
 
 class ShoppingList():
@@ -33,15 +32,12 @@ class ShoppingList():
         message = str(self)
         email.set_content(message)
 
-        try:
-            s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-            s.ehlo()
-            s.login(from_email, password)
-            s.send_message(email)
-            s.quit()
-            print("\nThe email has been sent.")
-        except Exception as e:
-            print("\nAn error occurred:", e)
+        s = SMTP_SSL('smtp.gmail.com', 465)
+        s.ehlo()
+        s.login(from_email, password)
+        s.send_message(email)
+        s.quit()
+        print("\nThe email has been sent.")
 
 
 def main():
@@ -49,13 +45,12 @@ def main():
     print(f"Hi, {name}!")
     shopping_list = ShoppingList()
     while True:
-            product = get_user_input("Input the product name (input \"stop\" when you are done): ")
-            if product == "stop":
-                break
-            quantity = get_user_input("Input the product quantity: ", int)
-            shopping_list.add_item(product, quantity)
-    print("\nThese products have been added to the shopping list:")
-    print(shopping_list)
+        product = get_user_input("Input the product name (input \"stop\" when you are done): ")
+        if product == "stop":
+            break
+        quantity = get_user_input("Input the product quantity: ", int)
+        shopping_list.add_item(product, quantity)
+    print(f"\nThese products have been added to the shopping list:\n{shopping_list}")
 
     email = input("\nEmail: ")
     password = getpass("Password: ")
